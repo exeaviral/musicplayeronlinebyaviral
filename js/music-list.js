7251b54fdff8997f01bcd817369102b0315cfeb9
exeaviral <111168855+exeaviral@users.noreply.github.com>
2022-10-10 13:23:42 +0530

    Initial Commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Heat Waves",
+    artist: "Glass Animals",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Gangsta's Paradise",
+    artist: "Coolio and Kylian Mash",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "No Love",
+    artist: "Shubh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hope!",
+    artist: "XXXTENTACION",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Come & Go",
+    artist: "Juice WRLD and Marshmello",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "295",
+    artist: "Sidhu Moose Wala",
+    img: "music-6",
+    src: "music-6"
+  },
+  ];
